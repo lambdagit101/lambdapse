c4ea78a378528e8be6e700dbe6cc0318d945b9fe
lambdagit101 <cevacinevus@gmail.com>
2021-03-09 11:55:26 +0200

    more testing

--- bot.js
+++ bot.js
@@ -145,6 +145,8 @@ client.player
 
 // Do not touch this
 
-// client.user.setActivity(messages.status_text, { type: messages.status_type })
+client.user.setActivity(messages.status_text, { type: messages.status_type })
+  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+  .catch(console.error);
 
 client.login(process.env.BOT_TOKEN);
