729c9962bfc90066a4d6c20940275085bc436d67
lambdagit101 <cevacinevus@gmail.com>
2021-05-11 18:41:23 +0300

    Fixed avatar command (message.chanenl lmfao) R.I.P. snek and c4 - you will be missed by some Getting activity invites from bots might soon be obsolete Changed Bot Status from Detroit: Become Human to Discord Poker Night

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -10,10 +10,10 @@ module.exports = {
   async execute(client, message, args) {
 		const user = message.mentions.users.first() || message.author;
 		const embed = new Discord.MessageEmbed()
-		        .setColor(require('../../messages.json').embed_color)
-		        .setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
-		        .setTimestamp()
-		        .setImage(user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-		message.chanenl.send(embed);
+		  .setColor(require('../../messages.json').embed_color)
+		  .setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
+		  .setTimestamp()
+		  .setImage(user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+		message.channel.send(embed);
   },
 };
