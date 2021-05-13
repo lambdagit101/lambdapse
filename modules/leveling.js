8ef58d0f79daea54f3c12cbce0398418c41cd4a8
lambdagit101 <cevacinevus@gmail.com>
2021-05-13 16:53:56 +0300

    An attempt to make the leveling system more like MEE6's Removed useless comments in youtube.js Tried making all activity-related commands have no invite preview, let's see it in action

--- modules/leveling.js
+++ modules/leveling.js
@@ -1,37 +1,71 @@
-module.exports.name = "Discord Leveling System"
+module.exports.name = "Discord Leveling System";
 
 // If set to true, leveling will work.
-const enabled = true
+const enabled = true;
 
 // If set to true, it will send a message when someone levels up
-const messagewhen = false
+const messagewhen = false;
 
 // Message that will be sent when someone levels up (does not matter if above is false)
-const theactualmessage = "**(TAG)**, you have advanced to level **(LEVEL)**!"
+const theactualmessage = '**(TAG)**, you have advanced to level **(LEVEL)**!';
+
+// Should the message when you got XP be sent in the same channel or a specific one? Not required if above is false
+const sendtochannel = false;
+const channelid = "required if above is true";
 
 // The Minimum and Maximum amount of XP someone can get per message
-const minxp = 1;
-const maxxp = 30;
+const minxp = 15;
+const maxxp = 25;
 
-// airpod shotty
+// Enable MEE6 type leveling? It will have a cooldown after every message
+const mee6leveling = true;
+
+// The MEE6 type leveling cooldown in miliseconds for when you can earn XP. Not required if above is false
+const mee6cooldown = 60000;
+
+// Do not touch the things below
+// skybird2333 chinaman
+
 var colors = require('colors');
-
+module.exports.enabled = enabled;
+if (!enabled) return console.log('[INFO]'.blue + ' Leveling module is ' + 'DISABLED'.red);
+if (enabled) console.log('[INFO]'.blue + ' Leveling module is ' + 'ENABLED'.green);
 const Levels = require("discord-xp");
 Levels.setURL(process.env.LEVEL_DBURL);
 module.exports.Levels = Levels;
-module.exports.enabled = enabled;
 const client = require('../bot.js').client;
 
-if (!enabled) return console.log('[INFO]'.blue + ' Leveling module is ' + 'DISABLED'.red);
-if (enabled) console.log('[INFO]'.blue + ' Leveling module is ' + 'ENABLED'.green);
 client.on("message", async (message) => {
   if (!message.guild) return;
   if (message.author.bot) return;
 
-  const randomAmountOfXp = Math.floor(Math.random() * (maxxp - minxp) + minxp);
-  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
-  if (hasLeveledUp) {
-    const user = await Levels.fetch(message.author.id, message.guild.id);
-    if (messagewhen) return message.channel.send(theactualmessage.replace('(TAG)', message.author.tag).replace('(LEVEL)', user.level));
+  if (mee6leveling && !message.author.cooldown) {
+    message.author.cooldown = true
+    const randomAmountOfXp = Math.floor(Math.random() * (maxxp - minxp) + minxp);
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.author.id, message.guild.id);
+      if (messagewhen) {
+        if (!sendtochannel) return message.channel.send(theactualmessage.replace('(TAG)', message.author.tag).replace('(LEVEL)', user.level));
+        const channel = client.channels.cache.get(channelid);
+        channel.send(theactualmessage.replace('(TAG)', message.author.tag).replace('(LEVEL)', user.level));
+      }
+    }
+    setTimeout(function() {
+      message.author.cooldown = false
+    }, mee6cooldown)
+  } else if (!mee6leveling) {
+    const randomAmountOfXp = Math.floor(Math.random() * (maxxp - minxp) + minxp);
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.author.id, message.guild.id);
+      if (messagewhen) {
+        if (!sendtochannel) return message.channel.send(theactualmessage.replace('(TAG)', message.author.tag).replace('(LEVEL)', user.level));
+        const channel = client.channels.cache.get(channelid);
+        channel.send(theactualmessage.replace('(TAG)', message.author.tag).replace('(LEVEL)', user.level));
+      }
+    }
+  } else if (mee6leveling && message.author.cooldown) {
+    return;
   }
 });
