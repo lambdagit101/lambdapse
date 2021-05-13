c3b8ba5072d2ff8547387889d7024e63bd47dd52
lambdagit101 <cevacinevus@gmail.com>
2021-05-13 17:50:59 +0300

    Added `system` command that returns little information about the system Might soon be improved on

---
+++ commands/Utility/system.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const os = require('os');
+
+module.exports = {
+	name: 'system',
+	aliases: ["specs"],
+	description: `Specifies the server's hardware.`,
+	emoji: ':bread:',
+	async execute(client, message, args) {
+		const embed = new Discord.MessageEmbed()
+			.setColor(require('../../messages.json').embed_color)
+			.setTimestamp()
+			.setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
+			.setTitle(require('../../messages.json').credits_title.replace('(NAME)', require('../../messages.json').bot_name))
+			.setDescription(`**\`${os.userInfo().username}@${os.hostname()}\`**\n**Platform: \`${os.platform()}\`**\n**Architecture: \`${os.arch()}\`**\n**RAM: \`${os.freemem()}B/${os.totalmem()}B\`**`)
+		message.channel.send(embed);
+	},
+};
