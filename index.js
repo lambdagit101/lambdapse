6742e72ad25ab449a7cccd7770f646c1b7ac148a
lambdagit101 <cevacinevus@gmail.com>
2021-05-19 15:04:26 +0300

    removed index comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Requires dotenv once, so it does not have to do it every time a shard is started
 require('dotenv').config();
 const colors = require('colors');
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./shard.js', { token: process.env.BOT_TOKEN });
 
-// Starting the created shard
 manager.on('shardCreate', shard => console.log('[STATUS]'.green + ` Launched shard ${shard.id}`));
 manager.spawn();
