24deebf97e3cb417a11b87331f491599ab66e297
Lyco Tatierra <frizth_tatierra@informatics.edu.ph>
2021-08-08 20:07:26 +0800

    Update youtube.js
    add button

--- commands/Music System/youtube.js
+++ commands/Music System/youtube.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'youtube',
-	aliases: ['dailymotion', 'vimeo', 'oniontube', 'cock'],
+	aliases: ['ytt', 'together'],
 	description: 'allows you to watch youtube with friends.\nnot that you have any.',
 	guildOnly: true,
 	emoji: ':arrow_forward:',
@@ -11,12 +11,16 @@ module.exports = {
 		if (!message.member.voice.channel) return message.channel.send(require('../../messages.json').music_notconnected);
 		client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
 			const embed = new Discord.MessageEmbed()
-				.setTitle('YouTube Together')
-				.setColor(require('../../messages.json').embed_color)
-				.setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
-				.setTimestamp()
-				.setDescription(`**[${require('../../messages.json').activity_clickhere}](${invite.code})**`)
-			return message.channel.send(embed);
+				.setTitle('Youtube Together')
+        			.setColor('#db1313')
+       	 			//.setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
+        			//.setTimestamp()
+        			.setDescription('**Click the button below to start**')
+      			let button = new MessageButton()
+        			.setStyle('url')
+        			.setURL(${invite.code}) 
+        			.setLabel('YT Together'); 
+      		return message.channel.send(embed, button);
 		});
 	},
 };
