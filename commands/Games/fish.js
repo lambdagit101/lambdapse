0379f5c5b58061ae7821c0c43fc1254464bc39d8
Lyco Tatierra <frizth_tatierra@informatics.edu.ph>
2021-08-08 19:59:50 +0800

    Update fish.js
    add button

--- commands/Games/fish.js
+++ commands/Games/fish.js
@@ -3,19 +3,23 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'fish',
   guildOnly: true,
-  description: 'fish',
+  description: 'fish fish',
   emoji: ':fish:',
   async execute(client, message, args) {
     if (require('../../modules/activities.js').enabled == false) return message.channel.send(require('../../messages.json').activity_disabled);
     if (!message.member.voice.channel) return message.channel.send(require('../../messages.json').music_notconnected);
     client.discordTogether.createTogetherCode(message.member.voice.channelID, 'fishing').then(async invite => {
       const embed = new Discord.MessageEmbed()
-        .setTitle('Fishington.io')
-        .setColor(require('../../messages.json').embed_color)
-        .setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
-        .setTimestamp()
-        .setDescription(`**[${require('../../messages.json').activity_clickhere}](${invite.code})**`)
-      return message.channel.send(embed);
+        .setTitle('Fish')
+        .setColor('#c5db13')
+        //.setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
+        //.setTimestamp()
+        .setDescription('**Click the button below to start**')
+      let button = new MessageButton()
+        .setStyle('url')
+        .setURL(${invite.code}) 
+        .setLabel('Fish'); 
+      return message.channel.send(embed, button);
     });
   },
 };
