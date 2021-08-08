32491b0309d47d72d81704d234346294746f4826
Lyco Tatierra <frizth_tatierra@informatics.edu.ph>
2021-08-08 20:04:02 +0800

    Update amogus.js

--- commands/Games/amogus.js
+++ commands/Games/amogus.js
@@ -1,9 +1,9 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js, discord-buttons');
 
 module.exports = {
-  name: 'amogus',
+  name: 'amongus clone',
   guildOnly: true,
-  aliases: ['betrayal'],
+  aliases: ['betrayal, amongus, amogus'],
   description: 'Sus.',
   emoji: ':fire_extinguisher:',
   async execute(client, message, args) {
@@ -11,12 +11,16 @@ module.exports = {
     if (!message.member.voice.channel) return message.channel.send(require('../../messages.json').music_notconnected);
       client.discordTogether.createTogetherCode(message.member.voice.channelID, 'betrayal').then(async invite => {
         const embed = new Discord.MessageEmbed()
-          .setTitle('Betrayal.io')
-          .setColor(require('../../messages.json').embed_color)
-          .setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
-          .setTimestamp()
-          .setDescription(`**[${require('../../messages.json').activity_clickhere}](${invite.code})**`)
-        return message.channel.send(embed);
+          .setTitle('Amongus Online (betrayal.io)')
+        .setColor('#db1313'
+        //.setFooter(require('../../messages.json').embed_footer.replace('(NAME)', message.author.username), message.author.avatarURL())
+        //.setTimestamp()
+        .setDescription('**Click the button below to start**')
+      let button = new MessageButton()
+        .setStyle('url')
+        .setURL(${invite.code}) 
+        .setLabel('Among Us'); 
+      return message.channel.send(embed, button);
       });
     },
 };
